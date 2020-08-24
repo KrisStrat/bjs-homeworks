80d4e9e5d041b205e5684634701f527414256d7b
KrisStrat <krisstrat@gmail.com>
2020-08-24 19:20:31 +0300

    Проверьте пожалуйста первую задачу по Типам Данных...почему он ругается я не понимаю.

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,63 @@
+
+let percent;
+let contribution;
+let amount;
+let date;
+let sumCredit;
+let month;
+let actualDate = new Date().getFullYear();
+let monthlyAmount;
+let totalAmount;
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (typeof percent == Boolean) {
+      return ` Параметр \" Процент \" содержит неправильное значение ${percent}`;
+    }
+    else  if (typeof contribution == Boolean){
+      return ` Параметр \" Начальный взнос \" содержит неправильное значение ${contribution}`;
+    }
+    else if (typeof amount == Boolean) {
+      return ` Параметр \" Сумма кредита \" содержит неправильное значение ${amount}`;
+    }
+    else if (typeof percent == Object) {
+      return ` Параметр \" Процент \" содержит неправильное значение ${percent}`;
+    }
+    else  if (typeof contribution == Object){
+      return ` Параметр \" Начальный взнос \" содержит неправильное значение ${contribution}`;
+    }
+    else if (typeof amount == Object) {
+      return ` Параметр \" Сумма кредита \" содержит неправильное значение ${amount}`;
+    }
+    else if (typeof percent == String) {
+      percent = parseInt(percent);
+      if (isNan(percent) === true) {  return ` Параметр \" Процент \" содержит неправильное значение ${percent}`;}
+    }
+    else  if (typeof contribution == String){
+      contribution = parseInt(contribution);
+      if (isNan(contribution) === true) {  return ` Параметр \" Начальный взнос \" содержит неправильное значение ${contribution}`;}
+    }
+    else if (typeof amount == String) {
+      amount = parseInt(amount);
+      if (isNan(amount) === true) {
+        return ` Параметр \" Сумма кредита \" содержит неправильное значение ${amount}`;
+    }
+    sumCredit = amount - contribution;
+    month = (date.getFullYear() - actualDate) * 12;
+    let p = percent / 12;
+    monthlyAmount = sumCredit * (p + p / (((1 + p)^month) - 1));
+    let fullSum = monthlyAmount * month;
+    totalAmount = fullSum.toFixed(2);
+    if (typeof totalAmount == Number) {
+      console.log(totalAmount);
+    }
+
+
+
+
+    return totalAmount;
 }
 
-function getGreeting(name) {
+//function getGreeting(name) {
     // код для задачи №2 писать здесь
     // return greeting;
-}
+//}
