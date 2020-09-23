62cf793dcade7b641366c564a5d2b8cac8bf749d
KrisStrat <krisstrat@gmail.com>
2020-09-23 18:58:28 +0300

    Задачи по обьектам и прототипам.

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,44 @@
-//String.prototype.isPalindrome - для задачи №1
+//Task 1
+let isPalindrome = function () {
+  let phrase = this.split('');
+  let phraseReverse = phrase.reverse();
+  if (phrase === phraseReverse) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+String.prototype = isPalindrome;
+
+String.prototype.isPalindrome() ;
+
+//Task 2
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sum = 0;
+    let averageMark;
+    let roundedAverage;
+    for (i = 0; i < marks.length ; i++) {
+      sum = sum + i;
+    }
+    averageMark = sum / marks.length;
+    roundedAverage = Math.round(averageMark);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = new Date(value);
+    let date ;
+    let birthday = +date;
+    let diff = now - birthday;
+    let age = diff / (365.25 * 24 * 60 * 60 * 1000);
+    if (age > 18) {
+      let verdict = console.log("Клиент является совершеннолетним.");
+    }
+    else {
+      let verdict = console.log("Не дорос еще!");
+    }
+
+    return verdict;
 }
