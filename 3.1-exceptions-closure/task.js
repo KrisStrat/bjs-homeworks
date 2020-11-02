3554695cb37a48e36a76ea516bb86b4d92b7ec65
Kris Stratu <krisstrat@MacBook-Pro-Refurb123.local>
2020-11-02 16:53:32 +0200

    задание по исключениям.во втором задании при проверку возвращает обьект со значениями undefind

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -2,48 +2,61 @@
 function parseCount(items) {
   let parseNumber = Number.parseInt(items);
   if (isNaN(parseNumber)) {
-    throw "Невалидное значение";
+    let errorItem = New Error("Невалидное значение.")
+    throw errorItem;
   }
   return parseNumber;
 }
 
 function validateCount(itemsData) {
   try {
-    let validData = parseCount(itemsData);
-    return validData;
+    return parseCount(itemsData);
   }
   catch(e) {
-    return new Error("Невалидное значение");
+    return errorItem;
   }
 }
 
 //task 2
-class Triangle {
-  constructor(a, b, c) {
+let a = Number(prompt("enter the number:"));
+let b = Number(prompt("enter the number:"));
+let c = Number(prompt("enter the number:"));
+class FakeTriangle {
+  constructor(a, b, c){
     this.first = a;
     this.second = b;
     this.third = c;
-    if (this.first + this.second <= this.third || this.second + this.third <= this.first || this.first + this.third <= this.second ) {
-      throw "Треугольник с такими сторонами не существует.";
+  }
+  getPerimeter() {
+    return "Ошибка!Треугольник не существует!";
+  }
+  getArea() {
+    return "Ошибка!Треугольник не существует!";
+  }
+}
+class Triangle extends FakeTriangle {
+  constructor(a, b, c) {
+    if (a + b <= c ||
+      b + c <= a ||
+      a + c <= b ) {
+      throw New Error ("Треугольник с такими сторонами не существует.");
     }
   }
-  getPerimetr() {
-    let perimetr = a + b + c ;
-    return perimetr;
+  getPerimeter() {
+    return this.first + this.second + this.third;
   }
 
   getArea() {
-    let p = (a + b + c) / 2;
-    let area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-    return area.toFixed(3);
+    let p = (this.first + this.second + this.third) / 2;
+    let area = Math.sqrt(p * (p - this.first) * (p - this.second) * (p - this.third));
+    return Number(area.toFixed(3));
   }
 }
- function getTriangle(a, b, c) {
+ function getTriangle() {
    try {
      return new Triangle;
    }
    catch(e) {
-
-
-   }
+     return new FakeTriangle;
+     }
  }
